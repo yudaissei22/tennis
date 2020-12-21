a2ae20982f3120aee8dffe4db5d042ef8d19ed56
Mirai Hattori <m-hattori@jsk.imi.u-tokyo.ac.jp>
2020-12-21 16:25:36 +0900

    update

--- scripts/make-algs.py
+++ scripts/make-algs.py
@@ -2,6 +2,8 @@
 import subprocess
 import os
 
+dry_run = True
+
 """
 algs = [
     "DIRECT",
@@ -34,6 +36,15 @@ motions = [
 
 margins = [
     "50.0",
+    "50.1",
+    "50.2",
+    "50.3",
+    "50.4",
+    "50.5",
+    "50.6",
+    "50.7",
+    "50.8",
+    "50.9",
 ]
 
 output_dir = "/userdir/logs/motion-planning"
@@ -41,18 +52,18 @@ os.makedirs(output_dir, exist_ok=True)
 # use config/p-orig.l or not
 config_p_value=True
 
-x_max = "5.0"
-x_hit = "3.0"
+x_max = "2.0"
+x_hit = "1.0"
 maxvel_weight = "1e-2"
 minjerk_weight = "3e0"
 x_step = "0.02"
-id_max = "20"
-recursive_order = "5"
-use_margin = "1"
+id_max = "18"
+recursive_order = "6"
+use_margin = "30"
 use_all_joint = "t"
 use_append_root_joint = "t"
-x_takeoff = "1.0"
-x_land = "2.0"
+x_takeoff = "0.4"
+x_land = "0.8"
 
 
 for alg in algs:
@@ -76,15 +87,16 @@ for alg in algs:
                             + " :use-all-joint " + use_all_joint \
                             + " :use-append-root-joint " + use_append_root_joint \
                             + ")\n")
-                    f.write("(setq *x-max-of-p-orig* " + x_max + ")")
-                    f.write("(setq *p-orig* (concatenate float-vector *ret* (float-vector " + x_takeoff + " " + x_land + " " + x_hit + ")))")
-                    f.write("(with-open-file (f \"/userdir/logs/p-orig.l\" :direction :output :if-exists :new-version) (format f \"~a\" *p-orig*))")
+                    f.write("(setq *x-max-of-p-orig* " + x_max + ")\n")
+                    f.write("(setq *p-orig* (concatenate float-vector *ret* (float-vector " + x_takeoff + " " + x_land + " " + x_hit + ")))\n")
+                    f.write("(with-open-file (f \"/userdir/logs/p-orig.l\" :direction :output :if-exists :new-version) (format f \"~a\" *p-orig*))\n")
                     f.write("(format t \"(boundp '*p-orig*) => ~A~%\" (boundp '*p-orig*))\n")
                 f.write("(comp::compile-file-if-src-newer \"nlopt_bspline_optimization.l\")\n")
                 f.write("(load \"nlopt_bspline_optimization.so\")\n")
                 f.write("(nlopt-init :x-max " + x_max + " :x-hit " + x_hit + " :id-max " + id_max + " :recursive-order " + recursive_order +  " :use-all-joint t :use-append-root-joint t :support-polygon-margin (list " + margin + " " + margin + " 0 100 50) :epsilon-c 30 :mu 0.3 :use-final-pose nil :default-switching-list nil :use-6dof-p t)\n")
                 f.write("(nlopt-motion-optimize :x-max " + x_max + " :x-hit " + x_hit + " :id-max " + id_max + " :recursive-order " + recursive_order + " :max-eval 10000000000 :alg " + alg + " :delta (deg2rad 0.01) :eqthre 1e-8 :xtol 1e-10 :ftol 1e-15 :use-all-joint t :use-margin 0.5 :use-append-root-joint t :maxvel-weight 1 :minjerk-weight 5e-4 :modify-ec t :p *p* :interval-num 20 :title \"maximize-speed\" :max-time (* 14 24 60 60) :file-path \"" + output_dir + "\")\n")
-                proc = subprocess.Popen(["roseus", "alg/" + motion + "/" + alg + "/" + margin + ".l"], shell=False, stdout=None, stderr=None)
+                if not dry_run:
+                    proc = subprocess.Popen(["roseus", "alg/" + motion + "/" + alg + "/" + margin + ".l"], shell=False, stdout=None, stderr=None)
                 # result = proc.communicate() # wait block
                 # (stdout, stderr) = (result[0], result[1])
                 # print(stdout)
---
+++ scripts/status.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import calendar
+import datetime
+import numpy as np
+in_dir = "/userdir/logs/motion-planning"
+
+# Feb-22-16-37-05-2019_forehand_maximize-speed_CCSA_48.0h_M-14_N-5_x-max-1.0_x-hit-0.5_maxvel-1_minjerk-0.0005_delta-1.7e-04_eqthre-1.0e-08_ftol-1.0e-15_xtol-1.0e-10_interval-20_sp-0-0-0-100-50_root-joint_modify-ec
+def parse_dirname(dir_name):
+    date_str = dir_name.split('_')[0]
+    try:
+        dt = datetime.datetime.strptime(date_str, '%b-%d-%H-%M-%S-%Y')
+    except ValueError as e:
+        dt = datetime.datetime.strptime(date_str, '%b--%d-%H-%M-%S-%Y')
+    motion_str = dir_name.split('_')[1]
+    target_str = dir_name.split('_')[2]
+    alg_str = dir_name.split('_')[3]
+    calctime_str = dir_name.split('_')[4]
+    id_max_str = dir_name.split('_')[5].split('-')[1]
+    recursive_order_str = dir_name.split('_')[6].split('-')[1]
+    x_max_str = dir_name.split('_')[7].split('-')[2]
+    x_hit_str = dir_name.split('_')[8].split('-')[2]
+    maxvel_str = dir_name.split('_')[9].split('-')[1]
+    minjerk_str = dir_name.split('_')[10].split('-')[1]
+    delta_str = dir_name.split('_')[11].split('-')[1]
+    eqthre_str = dir_name.split('_')[12].split('-')[1]
+    ftol_str = dir_name.split('_')[13].split('-')[1]
+    xtol_str = dir_name.split('_')[14].split('-')[1]
+    interval_str = dir_name.split('_')[15].split('-')[1]
+    sp_str = dir_name.split('_')[16].split('-')[1] # 5つあるけど最初のだけ考えている
+    return { \
+        "motion": motion_str, \
+        "target": target_str, \
+        "alg": alg_str, \
+        "calctime": calctime_str, \
+        "id_max": id_max_str, \
+        "recursive-order": recursive_order_str, \
+        "x_max": x_max_str, \
+        "x_hit": x_hit_str, \
+        "dt": dt, \
+        "sp": sp_str, \
+    }
+    # root_joint_str
+    # limb_joint_str
+    # modify_ec_str
+
+
+def parse_logfiles(log_dir):
+    with open(in_dir + "/" + log_dir + "/maximize-speed_eq.dat", "r") as f:
+        whole_lines = f.readlines()
+        if whole_lines:
+            eq = np.array([float(i) for i in whole_lines[-1].rstrip('\n').split()])
+        else:
+            eq = np.zeros(0)
+    with open(in_dir + "/" + log_dir + "/maximize-speed_ieq.dat", "r") as f:
+        whole_lines = f.readlines()
+        if whole_lines:
+            ieq = np.array([float(i) for i in whole_lines[-1].rstrip('\n').split()])
+            ieq[ieq < 0] = 0
+        else:
+            ieq = np.zeros(0)
+    return { \
+        "eq": eq, \
+        "ieq": ieq, \
+    }
+
+# os.scandir() might be better for python3.5 or upper
+dirs_sorted = sorted(os.listdir(in_dir), key=lambda s: parse_dirname(s)["dt"])
+
+for dirname in dirs_sorted:
+    attr = parse_dirname(dirname)
+    log = parse_logfiles(dirname)
+    print("{} "
+          "{} "
+          "{} "
+          "{} "
+          "{} "
+          "{} "
+          "{:<8} "
+          "{:8.1f} "
+          "{:9.1f} ".format(\
+                attr["dt"], \
+                attr["x_max"], \
+                attr["x_hit"], \
+                attr["id_max"], \
+                attr["recursive-order"], \
+                attr["alg"], \
+                attr["sp"], \
+                np.linalg.norm(log["eq"]), \
+                np.linalg.norm(log["ieq"]), \
+          )
+    )
